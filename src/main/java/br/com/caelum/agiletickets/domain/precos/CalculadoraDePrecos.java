fd64ed96791e34aa5f521d5f682fb70b8c1dbec8
Marcelo Eugenio <marceloeugenios@gmail.com>
2014-08-20 20:41:21 -0300

    Terceira refatoracao, refatoracao realiza nos metodos de calculos

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -12,29 +12,29 @@ public class CalculadoraDePrecos {
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
 			if(verificarQuantidadeIngressos(sessao, 0.05)) { 
-				preco = calculaPreco(sessao, 0.10);
+				preco = calculaPrecoSessao(sessao, 0.10);
 			} else {
 				preco = sessao.getPreco();
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
 			if(verificarQuantidadeIngressos(sessao, 0.50)) { 
-				preco = calculaPreco(sessao, 0.20);
+				preco = calculaPrecoSessao(sessao, 0.20);
 			} else {
 				preco = sessao.getPreco();
 			}
 			
 			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = preco.add(multiplicaPrecoSessao(sessao,0.10));
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
 			if(verificarQuantidadeIngressos(sessao, 0.50)) { 
-				preco = calculaPreco(sessao, 0.20);
+				preco = calculaPrecoSessao(sessao, 0.20);
 			} else {
 				preco = sessao.getPreco();
 			}
 
 			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				preco = preco.add(multiplicaPrecoSessao(sessao,0.10));
 			}
 		}  else {
 			//nao aplica aumento para teatro (quem vai é pobretão)
@@ -43,8 +43,12 @@ public class CalculadoraDePrecos {
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
-	private static BigDecimal calculaPreco(Sessao sessao, double multiplicador) {
-		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(multiplicador)));
+	private static BigDecimal multiplicaPrecoSessao(Sessao sessao, double multiplicador) {
+		return sessao.getPreco().multiply(BigDecimal.valueOf(multiplicador));
+	}
+
+	private static BigDecimal calculaPrecoSessao(Sessao sessao, double multiplicador) {
+		return sessao.getPreco().add(multiplicaPrecoSessao(sessao, multiplicador));
 	}
 
 	private static boolean verificarQuantidadeIngressos(Sessao sessao, double limite) {
